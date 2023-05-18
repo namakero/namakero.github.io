0ea66b6589ddca81d80b2fe45e6ae3c32635cf23
namakero <namakero11@gmail.com>
2023-05-18 17:47:00 +0900

    a

---
+++ index.js
@@ -0,0 +1,11 @@
+const h1Collection = document.getElementsByTagName("h1")
+console.log(h1Collection)
+
+const hogeCollection = document.getElementsByClassName("hoge")
+console.log(hogeCollection)
+
+const fuga = document.getElementById("fuga")
+console.log(fuga)
+
+const p = document.createElement('p')
+console.log(p)
