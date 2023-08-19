60074609d52180a847e97e7f6b7215306ede1a33
なまけろ <130877458+namakero@users.noreply.github.com>
2023-08-19 12:48:23 +0900

    Delete hub directory

--- hub/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import counter from './components/Counter.vue'
-import todolist from './components/ToDoList.vue'
-
-createApp(counter).mount('#vueCounter');
-createApp(todolist).mount('#vueToDoList')
-
